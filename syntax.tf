a9b5c6498af6f79a481f8a2c7109b2c3e3c4ddd6
Arati Karpe <karpearati76@gmail.com>
2026-06-09 18:05:38 +0530

    Update syntax.tf

--- syntax.tf
+++ syntax.tf
@@ -1,15 +1,17 @@
 provider "aws" {
-  region  = "us-east-2"
-  profile = "default"
+  region = "us-east-2"
+  profile = "configs"
 }
 
-resource "aws_instance" "webserver" {
-  ami                    = "ami-0c6ac5f2fed2981b0"
-  instance_type          = "t2.micro"
-  key_name               = "sample"
-  vpc_security_group_ids = ["sg-051c13ca487ff9c4f"]
 
+resource "aws_instance" "webserver" {
+  ami                    = var.this_ami
+  instance_type          = var.This_instance_type
+  key_name               = "var.this_key_name"
+  vpc_security_group_ids = [var.This_vpc_security_group_ids]
+  count = var.this_count
   tags = {
     Name = "webserver"
   }
+   disable_api_termination = var.this_disable_Api_termination
 }
