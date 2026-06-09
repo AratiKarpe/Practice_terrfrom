7254d8b120a310c78eb50589c1a50d5aa4c2d9ec
Arati Karpe <karpearati76@gmail.com>
2026-06-09 18:08:10 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,2 +1,25 @@
 variable "this_key_name" {
-  default = ""
+  default = "sample"
+
+}
+
+variable "this_ami" {
+  default = "ami-0c6ac5f2fed2981b0"
+}
+
+variable This_instance_type" {
+  default = "t3.micro"
+
+}
+
+variable "This_vpc_security_group_ids" {
+  default = "sg-051c13ca487ff9c4f"
+}
+
+variable "this_count" {
+  default = 4
+}
+
+variable "this_disable_Api_termination" {
+  default = true
+}
