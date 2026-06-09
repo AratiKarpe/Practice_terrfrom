e89fe0ca0311972b843ede97ee8175e155c79c5c
Arati Karpe <karpearati76@gmail.com>
2026-06-09 15:10:07 +0530

    Update syntax.tf

--- syntax.tf
+++ syntax.tf
@@ -1,3 +1,19 @@
 provider "aws" {
   region = "us-east-1"
+  profile = "configs"
+  access_key = "my-access_key"
+  secrete_key = "my-secrete_key"
 }
+
+resource "aws_instance" "webserver"{
+  key_name =
+  ami =
+  instance_type =
+  vpc_security_group_ids =
+  disable_api_termination =
+  tags ={
+    Name = "webserver"
+  }
+}
+
+aws configure --profile=configs
